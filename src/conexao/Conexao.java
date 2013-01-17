48d1e50253174f57b6a1f6e3ed3411a0b117118b
Jéssica Adamy <jessica.adamy@gmail.com>
2013-01-17 11:15:18 -0300

    commit

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Conexao {
+public class Conexao { 
 	
 	private static Connection sqlConn = null, sqlConnAux = null, sqlConnConsulta = null;
 	private static Connection mysqlConn = null, mysqlConnAux = null;
